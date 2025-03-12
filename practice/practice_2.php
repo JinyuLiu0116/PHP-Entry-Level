ca9bf330ac2840ae414c1d332ce4dbf066a71eed
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-03-11 21:55:55 -0400

    Update practice_2.php

--- practice/practice_2.php
+++ practice/practice_2.php
@@ -1,1 +1,9 @@
-
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            $myName = "My name";
+            $firstName = "Jinyu";
+            $lastName = "Liu";
+            echo "My first PHP script!<br>{$myName} is: {$firstName} {$lastName}<br>";
+            echo gettype($myName);
