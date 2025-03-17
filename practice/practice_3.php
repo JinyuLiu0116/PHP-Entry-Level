d1624ad1d49ac61deaee475cc2c03a2a020bc2dd
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-03-17 15:27:30 -0400

    Update practice_3.php

--- practice/practice_3.php
+++ practice/practice_3.php
@@ -1,1 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <form action="practice01.php" method="post">
+            <label>Quantity</label><br>
+            <input type="text" name="quantity"><br>
+            <input type="submit" value="Total"><br>
+        </form>
+    </body>
+</html>
+<?php
+    $price = 3.99;
+    $quantity = $_POST["quantity"];
+    $total = null;
+    $total = $price * $quantity;
 
+    echo "You have {$quantity}/s iteam with price:\${$price}<br>";
+    echo "Your total is:\${$total}";
+
+?>
