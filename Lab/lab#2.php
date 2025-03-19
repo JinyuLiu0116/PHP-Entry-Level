a6b4beb07cc66746c7570f588094840bf9d356fe
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-03-18 21:09:46 -0400

    Update lab#2.php

--- Lab/lab#2.php
+++ Lab/lab#2.php
@@ -12,3 +12,14 @@
     echo '$strValue:'.var_dump($strValue).'<br>';
     echo '$boolValue:'.var_dump($boolValue).'<br>';
     echo '$nullValue:'.var_dump($nullValue).'<br>';
+    /*
+    Questions#2: Create a script where you add an integer and a float. 
+    Display the result and explain the type conversion that happens internally.
+    */
+    echo "----------------------------------------------<br>";
+    $intNum = 100;
+    $floatNum = 100.00;
+    $result = $intNum + $floatNum;
+    echo ' $result:'.var_dump($result).'<br>';
+    //The value of $result is 200 and the type is float because the type of $intNum is converted to float
+    echo "----------------------------------------------<br>";
