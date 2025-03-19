0cfcd725391302539bf7385cc1c1a36525c59b65
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-03-18 21:09:33 -0400

    Update lab#2.php

--- Lab/lab#2.php
+++ Lab/lab#2.php
@@ -1,1 +1,14 @@
-
+<?php
+    /*Questions#1: Create a PHP script that initializes variables with different data types 
+    (integer, float, string, boolean, array, NULL). Use the var_dump() function 
+    to display the data type and value of each variable.*/
+    $intValue = 111;
+    $floValut = 111.111;
+    $strValue = "string";
+    $boolValue = true;
+    $nullValue = NULL;
+    echo '$intValue:'.var_dump($intValue).'<br>';
+    echo '$floValut:'.var_dump($floValut).'<br>';
+    echo '$strValue:'.var_dump($strValue).'<br>';
+    echo '$boolValue:'.var_dump($boolValue).'<br>';
+    echo '$nullValue:'.var_dump($nullValue).'<br>';
