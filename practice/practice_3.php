503a4784089753ce829d114e621490705f85ba77
Jinyu Liu <149179560+JinyuLiu0116@users.noreply.github.com>
2025-03-18 21:08:44 -0400

    Update practice_3.php

--- practice/practice_3.php
+++ practice/practice_3.php
@@ -18,3 +18,41 @@
     echo "Your total is:\${$total}";
 
 ?>
+<?php
+$a = 5;
+$b = 5.34;
+$c = "25";
+var_dump($a);
+echo "<br>";
+var_dump($b);
+echo "<br>";
+var_dump($c);
+echo "<br>";
+// Check if the type of a variable is integer   
+$x = 5985;
+var_dump(is_int($x));
+
+echo "<br>";
+
+// Check again... 
+$x = 59.85;
+var_dump(is_int($x));
+echo "<br>";
+
+echo strlen("Hello world!"); // Output: 12
+echo "<br>";
+
+echo str_word_count("Hello world!");  // Output: 2
+echo "<br>";
+
+echo strrev("Hello"); // Output: olleH
+echo "<br>";
+
+echo strpos("Hello World!", "World"); // Output: 6
+echo "<br>";
+
+$x = '25555x6';
+$y = (integer) $x; // cast $x to int
+var_dump($y);
+
+?>
